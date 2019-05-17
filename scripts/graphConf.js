787dfba6183827b1c676771f8c697777f4d81c04
Filipe Teixeira <fppintoteixeira@gmail.com>
2019-05-17 14:40:26 +0200

    Increase timeouts

--- scripts/graphConf.js
+++ scripts/graphConf.js
@@ -10,7 +10,7 @@ const host = false;
 const SINGLE_COMMANDS_AND_NO_VARS = false;
 
 // Time out for the REST protocol. Increase it if the graphDB is slow.
-const REST_TIMEOUT = 2000 
+const REST_TIMEOUT = 120000 
 // TODO: configuration for the secure server
 
 
